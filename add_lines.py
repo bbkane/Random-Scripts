5978feb8bf335f64cee842a503993fbf0c5fc0c6
Benjamin Kane <bbk1524@gmail.com>
2018-12-22 20:26:37 -0800

    Syntax change, tests

--- add_lines.py
+++ add_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from textwrap import dedent
 import argparse
 import re
 import statistics
@@ -9,15 +10,15 @@ import sys
 __author__ = "Benjamin Kane"
 __version__ = "0.1.0"
 __doc__ = f"""
-Count lines like: <marker> <name> <amount>  # optional comment
+Count lines like: <marker> <name> , <amount>  # optional comment
 By default <marker> is ;;
 
 The intent for this script is to be able to calculate simple stats on notes by
 including lines like the above interspersed in text files
 
 Examples:
-    `{sys.argv[0]}` text.txt
-    `{sys.argv[0]}` < text.txt
+    {sys.argv[0]} text.txt
+    {sys.argv[0]} < text.txt
 
 Help:
 Please see Benjamin Kane for help.
@@ -53,21 +54,27 @@ def parse_args(*args, **kwargs):
         action='store_true',
         help='Print numeric lines as found'
     )
+    parser.add_argument(
+        '--testing',
+        '-t',
+        action='store_true',
+        help='Run some quick tests'
+    )
 
     return parser.parse_args(*args, **kwargs)
 
 
-def main():
-    args = parse_args()
-    # args = argparse.Namespace(infile=open('tmp.txt'), marker=';;')
+def match_line(line, marker):
     pattern = re.compile(
-        f"^{args.marker}" + r"""\s+
+        f"^{marker}" + r"""\s+
         (?P<name>\w+)
-        \s+
+        \s*
+        ,
+        \s*
         (?P<amount>
             [+-]?             # optional signe
             [0-9]+            # some digits
-            (\.[0-9]+)?       # optional decimal part
+            (\.[0-9]*)?       # optional decimal part
         )
         \s*                   # can end in whitespace
         (
@@ -77,11 +84,40 @@ def main():
         \n""",
         re.VERBOSE
     )
+    match = re.match(pattern, line)
+    if match:
+        return match.groupdict()
+    return None
+
+
+def test_match_lines():
+    """Poor man's in-file pytest"""
+    lines = dedent("""
+    ;; A , 1
+    ;; B , 1.1
+    ;; C,1
+    ;; D, 1
+    ;; E ,1
+    ;; F ,-1
+    """).strip()
+
+    for line in lines.split('\n'):
+        line += '\n'
+        print(f'Testing: {repr(line)}')
+        d = match_line(line, ';;')
+        print(f'Result: {repr(d)}')
+        print()
+
+
+def main():
+    args = parse_args()
+    if args.testing:
+        test_match_lines()
+        return
     numbers = []
     for line in args.infile:
-        match = re.match(pattern, line)
-        if match:
-            d = match.groupdict()
+        d = match_line(line, args.marker)
+        if d:
             if args.verbose:
                 print(d)
             numbers.append(float(d['amount']))
