5463f3c8237cfccdc57a230d241f248df4056f14
Benjamin Kane <bbkane@users.noreply.github.com>
2020-02-07 17:43:32 -0800

    Update zsh_prompt.zsh

--- zsh_prompt.zsh
+++ zsh_prompt.zsh
@@ -184,3 +184,22 @@ autoload -Uz compinit && compinit
 
 # TODO: https://github.com/clvv/fasd
 
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+function colored() {
+	command env \
+		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;31m") \
+		LESS_TERMCAP_me=$(printf "\e[0m") \
+		LESS_TERMCAP_se=$(printf "\e[0m") \
+		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+		LESS_TERMCAP_ue=$(printf "\e[0m") \
+		LESS_TERMCAP_us=$(printf "\e[1;32m") \
+		PAGER="${commands[less]:-$PAGER}" \
+		_NROFF_U=1 \
+		PATH="$HOME/bin:$PATH" \
+			"$@"
+}
+
+function man() {
+	colored man "$@"
+}
